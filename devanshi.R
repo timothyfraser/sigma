4faf17db3b55d61b5269f56af49f483c16f7712d
Timothy Fraser <timothy.fraser.1@gmail.com>
2023-09-18 11:56:29 -0400

    .

---
+++ devanshi.R
@@ -0,0 +1,14 @@
+# devanshi.R
+# Goals
+
+# Check out timothyfraser.com/sigma
+
+# 1. An example of Bayes Rule - not using my existing code or chapters.
+
+# 2. A nice example of fault tree simulation
+# --- not using tidy fault
+# --- hey, here's this function f(t, rnode1, rnode2){  }
+
+# 3. Chi-squared 
+
+# 4. Typo typo typo typo -- I don't that's right.
